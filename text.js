0fcfb51d4c976b8f35526197b991f4e0cfcbcd63
eniks <olaniyieniikeoluwa@gmail.com>
2022-08-13 02:12:46 +0100

    added index and js files

---
+++ text.js
@@ -0,0 +1,61 @@
+//while loop
+// var age = 5;
+
+// while (age < 10){
+//     console.log("your age is less than 10");
+//     age++;
+// }
+// document.write("your age is less than 10");
+
+//for loop
+// for (age = 1; age < 10; age++){
+//    console.log("your age is less than 10");
+//  }
+//  document.write("your age is less than 10");
+
+//functions
+// function getAverage (a,b) {
+//     var average = (a + b) / 2;
+//     console.log(average);
+// }
+// var myResult = getAverage(7,12);
+// console.log("the average is " +myResult);
+
+//string
+// var string1 = "i am a string";
+// var string2 = "abc";
+
+// console.log(string1 < string2);
+
+//array
+//var myArray = new Array();
+// var myArray = [];
+// myArray[0] = 25
+// myArray[1] = 35
+// myArray[2] = 45
+// console.log(myArray);
+
+//constructor functions
+// var Car = function(maxSpeed, driver) {
+//     this.maxSpeed = maxSpeed;
+//     this.driver = driver;
+//     this.drive = function(speed, time) {
+//         console.log(speed * time);
+//     };
+//     this.logDriver = function(){
+//         console.log("driver name is " +this.driver);
+//     };
+
+// }
+// var myCar = new Car (70, "Eniks"); 
+
+
+
+
+
+
+
+
+
+
+
